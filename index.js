96069ee296eb2f6b8b6148366e09176f632f89e8
Leonardo Alvarez <leo92alvarez25@gmail.com>
2023-09-12 01:09:48 -0300

    connection to the database

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const dotenv = require('dotenv')
 const express = require('express');
+const helmet = require('helmet');
+const { Sequelize } = require('sequelize');
 
 
 dotenv.config();
@@ -7,6 +9,20 @@ const app = express();
 app.use(helmet());
 
 const port = process.env.PORT || 3000;
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos establecida con éxito.');
+  })
+  .catch((error) => {
+    console.error('Error al conectar a la base de datos:', error);
+  });
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
